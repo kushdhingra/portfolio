81aeb4cf5d33aed3b07c859687cff32fa9517c19
Kush-Dhingra <kushdhingra3112@gmail.com>
2024-12-10 21:31:29 +0530

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",   // Unsplash
-      "cdn.pixabay.com",       // Pixabay
-      "images.pexels.com",     // Pexels
-      "raw.githubusercontent.com", // raw GitHub content
-      "*.githubusercontent.com",   // all GitHub user content
-      "http.cat"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "http.cat",
+      },
     ],
   },
 };
